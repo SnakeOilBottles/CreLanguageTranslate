37df6a06ff067f3ec98b0e061fec23cd5dcc636a
KMicha <michael.kahle@yahoo.de>
2025-11-09 15:52:56 +0100

    print version

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 
 from CreLanguageTranslate.LanguageTranslate import LanguageTranslate 
+import CreLanguageTranslate 
+from importlib.metadata import version
 
 class TestTranslate(TestCase):
+
+    def test_version(self):
+        print(['CreLanguageTranslate: ', version("CreLanguageTranslate")])
+        self.assertEqual('Version', 'Version')
+
     def test_simple_translate(self):
         lt = LanguageTranslate()
         li = lt.getTranslatorByLanguage('en','de')
