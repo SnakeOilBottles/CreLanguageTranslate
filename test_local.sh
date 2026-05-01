19078bbb54e94c400db6102b72af4edeb02c9c5c
KMicha <michael.kahle@yahoo.de>
2026-05-01 11:32:00 +0200

    use secrets

--- test_local.sh
+++ test_local.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 pip3 install ../CreLanguageTranslate/
+
+if [ ! -f mysecrets.py ]; then
+  cp mysecrets.orig.py mysecrets.py
+fi
+
 python3 -m unittest
 pip3 uninstall -y CreLanguageTranslate
 pip3 install CreLanguageTranslate
