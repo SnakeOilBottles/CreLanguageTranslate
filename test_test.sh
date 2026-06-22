4e2ed0a4d0d83f156b7b33288e468b1a8cff2deb
KMicha <michael.kahle@yahoo.de>
2026-06-22 12:02:37 +0200

    upgrade to latest version

--- test_test.sh
+++ test_test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-python -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple CreLanguageTranslate
+#python -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple CreLanguageTranslate
+python -m pip install --upgrade --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple CreLanguageTranslate
 
 if [ ! -f mysecrets.py ]; then
   cp mysecrets.orig.py mysecrets.py
