228487ca842f67c0e4d5e40e289a8ec1c7e891a9
KMicha <michael.kahle@yahoo.de>
2026-09-14 13:29:56 +0200

    sleep more

--- src/CreLanguageTranslate/dTgoogleTranslate.py
+++ src/CreLanguageTranslate/dTgoogleTranslate.py
@@ -69,9 +69,9 @@ class dTgoogleTranslate(TranslateBase):
         anySource = random.choice(dTgoogleTranslate.isoDictionary[sourceLanguage])
         anyTarget = random.choice(dTgoogleTranslate.isoDictionary[targetLanguage])  
         gt = GoogleTranslator(source=anySource, target=anyTarget) 
-        time.sleep(0.3)
+        time.sleep(3)
         targetText = gt.translate(sourceText)
-        time.sleep(0.3)
+        time.sleep(3)
         # dTgoogleTranslate.isWorking = False
         return targetText
 
